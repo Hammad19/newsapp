d60f39bf74c142acf02e6d19d5693e05fe29731c
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-09-21 01:07:55 +0500

    Fixes width of cards

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function Newsitem(props) {
   return (
-
-    <div className="card" style={{width: "18rem"}}>
-    <img src={props.imageurl} className="card-img-top" alt="..."/>
-    <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">{props.description}</p>
-        <a href="/" className="btn btn-primary btn btn-sm">Read More</a>
-    </div>
+    <div className='my-3'>
+      <div className="card" style={{width: "18rem"}}>
+      <img src={props.imageurl} className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}</p>
+            <a href={props.newsurl} target = "_blank" className="btn btn-primary btn btn-sm">Read More</a>
+        </div>
+      </div>
     </div>
   )
 }
