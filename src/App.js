1ccb676c1ceda46fcae7c469b828d0bfa69689a5
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-09-22 14:21:12 +0500

    made page size dynamic

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import News from './components/News';
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
+
+  const pagesize = 20;
   return (
     <>
      <Navbar></Navbar>
      <Routes>
-    <Route path = "/About" element= {<News category = 'sports' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "in" headlines ='NewsMania - Top Headlines' pageSize = {8}/>}></Route>
+    <Route exact path = "/" element= {<News key = 'business' category = 'business' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/business" element= {<News key = 'business' category = 'business' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
+    
+    
+    <Route exact path = "/entertainment" element= {<News key = 'entertainment' category = 'entertainment' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/general" element= {<News key = 'general' category = 'general' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/health" element= {<News key = 'health' category = 'health' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/science" element= {<News key = 'science' category = 'science' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/sports" element= {<News key = 'sports' category = 'sports' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {29}/>}></Route>
+    <Route exact path = "/technology" element= {<News key= 'technology' category = 'technology' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
     
     </Routes>
     </>
