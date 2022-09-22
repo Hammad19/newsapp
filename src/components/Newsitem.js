39436a033c0d3b3955fe22260285eee00de322aa
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-09-22 14:22:14 +0500

    Added Author and Time of News

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
 export default function Newsitem(props) {
+
+  
   return (
     <div className='my-3'>
+      
+
+      <span class="badge badge-dark">Dark</span>
       <div className="card" style={{width: "18rem"}}>
+        
       <img src={props.imageurl} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{props.title}...</h5>
+            <h5 className="card-title">{props.title}<span class="badge badge-dark">Dark</span></h5>
             <p className="card-text">{props.description}...</p>
-            <a href={props.newsurl} target = "_blank" className="btn btn-primary btn-sm">Read More</a>
+            <p className="card-text"><small className="text-muted">By {props.author?props.author:"Unknown"} On {new Date(props.date).toGMTString()}</small></p>
+            <a href={props.newsurl} target = "_blank" className="btn btn-dark btn-sm">Read More</a>
+            
         </div>
       </div>
     </div>
