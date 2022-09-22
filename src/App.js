a1671a74842193c8a92d8563126e500a599cddbc
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-09-22 16:19:57 +0500

    imported and setup infinite scroll and loading bar

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
 import {Routes,Route} from 'react-router-dom'
+import { useState } from 'react';
+import LoadingBar from 'react-top-loading-bar'
 
 function App() {
 
-  const pagesize = 20;
+
+  const[progress,setProgress] = useState(0);
+  const pagesize = 6;
   return (
     <>
+
+      <LoadingBar
+        color='#f11946'
+        progress={progress}
+        height={3}
+        
+      />
      <Navbar></Navbar>
      <Routes>
-    <Route exact path = "/" element= {<News key = 'business' category = 'business' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
-    <Route exact path = "/business" element= {<News key = 'business' category = 'business' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/" element= {<News setProgress= {setProgress} key = 'business' category = 'business' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us"  pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/business" element= {<News setProgress= {setProgress} key = 'business' category = 'business' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us"  pagesize = {pagesize}/>}></Route>
     
     
-    <Route exact path = "/entertainment" element= {<News key = 'entertainment' category = 'entertainment' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
-    <Route exact path = "/general" element= {<News key = 'general' category = 'general' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
-    <Route exact path = "/health" element= {<News key = 'health' category = 'health' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
-    <Route exact path = "/science" element= {<News key = 'science' category = 'science' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
-    <Route exact path = "/sports" element= {<News key = 'sports' category = 'sports' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {29}/>}></Route>
-    <Route exact path = "/technology" element= {<News key= 'technology' category = 'technology' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us" headlines ='NewsMania - Top Headlines' pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/entertainment" element= {<News setProgress= {setProgress} key = 'entertainment' category = 'entertainment' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us"  pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/general" element= {<News setProgress= {setProgress} key = 'general' category = 'general' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us"  pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/health" element= {<News setProgress= {setProgress} key = 'health' category = 'health' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us"  pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/science" element= {<News setProgress= {setProgress} key = 'science' category = 'science' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us"  pagesize = {pagesize}/>}></Route>
+    <Route exact path = "/sports" element= {<News setProgress= {setProgress} key = 'sports' category = 'sports' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us"  pagesize = {29}/>}></Route>
+    <Route exact path = "/technology" element= {<News setProgress= {setProgress} key= 'technology' category = 'technology' apiKey ='0eac8158287f4625aae58d2ad60e12fe'  country= "us"  pagesize = {pagesize}/>}></Route>
     
     </Routes>
     </>
--- src/components/News.js
+++ src/components/News.js
@@ -2,68 +2,57 @@ import React from 'react'
 import Newsitem from './Newsitem'
 import { useState,useEffect } from 'react'
 import Spinner from './Spinner';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 
 export default function News(props) {
 
-     let articls = [
-          {
-              "source": {
-                  "id": "espn-cric-info",
-                  "name": "ESPN Cric Info"
-              },
-              "author": null,
-              "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-              "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-              "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-              "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-              "publishedAt": "2020-04-27T11:41:47Z",
-              "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-          },
-          {
-              "source": {
-                  "id": "espn-cric-info",
-                  "name": "ESPN Cric Info"
-              },
-              "author": null,
-              "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-              "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-              "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-              "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-              "publishedAt": "2020-03-30T15:26:05Z",
-              "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-          }
-      ];
+     let articls = [];
      const [articles, setarticles] = useState(articls);
-     const [loading,setloading] = useState(false);
+     const [loading,setloading] = useState(true);
      const [page,setPage] = useState(1);
      const[totalresults,setTotalResults] = useState(0);
      
      const updatenews = async ()=>
      {
+
+          props.setProgress(10);
           setloading(true);
           let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pagesize}`;
           let data = await fetch(url);
+          props.setProgress(40);
           let parsedData = await data.json();
+          props.setProgress(70);
           //console.log(parsedData);
           setarticles(parsedData.articles);
           setTotalResults(parsedData.totalResults);
           setloading(false);
           //console.log(totalresults)
+          props.setProgress(100);
+          document.title = `${capitalizedFirsLetter(props.category)} - NewsMania`;
 
      } 
-     const handleprevious = async ()=>
+
+     const capitalizedFirsLetter = (string) =>
      {
-          
-          setPage(page-1);
-          updatenews();
-          
-     } 
+      
+      return string.charAt(0).toUpperCase() + string.slice(1);
+      
 
-     const handlenext = async ()=>
+     }
+  
+     const fetchMoreData = async ()=>
      {     
-               setPage(page+1);
-               updatenews();
+              setPage(page+1);
+              setloading(true);
+              let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pagesize}`;
+              let data = await fetch(url);
+              let parsedData = await data.json();
+              setarticles(articles.concat(parsedData.articles));
+              setTotalResults(parsedData.totalResults);
+              setloading(false);
+              
         
      } 
      useEffect (()=>
@@ -72,25 +61,35 @@ export default function News(props) {
      }, [])
 
   return (
-    <div className='container my-3'>
-        <h1  style ={{margin: '40px'}}className='text-center'>{props.headlines}</h1>
-        <Spinner loading = {loading}></Spinner>
+    <>
+        <h1  style ={{margin: '40px'}}className='text-center'>NewsMania - Top {capitalizedFirsLetter(props.category)} Headlines</h1>
+        {loading && <Spinner/>}
+
+
+        <InfiniteScroll
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length !== totalresults}
+          loader={<Spinner loading = {loading}></Spinner>}
+        >
+        <div className="container">
         <div className="row d-flex justify-content-center ">
+        
+        
         {articles.map((element)=>
         {
-          return !loading &&<div className="col md-4" key = {element.url}>
+          return <div className="col md-4" key = {element.url}>
               <Newsitem  title={element.title?element.title.slice(0,40):""} 
               description={element.description?element.description.slice(0,88):""} 
               imageurl = {element.urlToImage?element.urlToImage:""} 
               newsurl = {element.url} author={element.author} 
               date={element.publishedAt}></Newsitem>
+              
           </div>
           })}
+          </div>
         </div>
-          <div className='container d-flex  justify-content-between'>
-            <button disabled = {page<=1} onClick={handleprevious} type="button" className="btn btn-dark">&laquo; Previous</button>
-           <button disabled = {page +1 > Math.ceil(totalresults/props.pagesize)} onClick={handlenext} type="button" className="btn btn-dark">Next &raquo;</button>
-        </div>
-    </div>
+        </InfiniteScroll>
+        </>
   )  
 }
